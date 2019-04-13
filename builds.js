655126822467a1d6fa8a313231ec7e5d42ac1b7b
Marotheit <Marotheit@users.noreply.github.com>
2019-04-13 16:01:37 -0600

    Added error handling for builds that don't exist
    Now, when a build is selected that doesn't exist, an "error" message is displayed, and the user is offered the option to create the build themselves.

--- builds.js
+++ builds.js
@@ -10,6 +10,10 @@ $(document).ready(function(){
 		jQuery.get(results, function(data){
 			$('#relevant_build').html(data);
 			$('#relevant_build').css("display", "block");
+		})
+		.fail(function (err){
+			$('#relevant_build').html("<div id=\"sorry\">Sorry!</div><div id=\"not_found\">It seems like this guide hasn't been implemented quite yet!<br/><br/>Please check back later, or, if you're feeling adventurous, <a href=\"https://github.com/Marotheit/Terraria-at-a-Glance\">feel free to write it yourself!</a></div>");
+			$('#relevant_build').css("display", "block");
 		});
 	});
 });
