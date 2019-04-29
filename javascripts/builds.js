efa6c9b34950af8f637891bab9b0d88373de93eb
Marotheit <Marotheit@users.noreply.github.com>
2019-04-29 12:58:54 -0600

    Bug fixes for builds, stylesheets, and builds.js

--- javascripts/builds.js
+++ javascripts/builds.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			$('#relevant_build').css("display", "block");
 		})
 		.fail(function (err){
-			$('#relevant_build').html("<div id=\"sorry\">Sorry!</div><div id=\"not_found\">It seems like this guide hasn't been implemented quite yet!<br/><br/>Please check back later, or, if you're feeling adventurous, <a href=\"https://github.com/Marotheit/Terraria-at-a-Glance/wiki/Creating-a-Build">feel free to write it yourself!</a></div>");
+			$('#relevant_build').html("<div id=\"sorry\">Sorry!</div><div id=\"not_found\">It seems like this guide hasn't been implemented quite yet!<br/><br/>Please check back later, or, if you're feeling adventurous, <a href=\"https://github.com/Marotheit/Terraria-at-a-Glance/wiki/Creating-a-Build\">feel free to write it yourself!</a></div>");
 			$('#relevant_build').css("display", "block");
 		});
 	});
